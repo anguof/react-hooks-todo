bce2fa021c740ca01530f74c53c856d7820de84b
anguo <guo.an@surpath.net.cn>
2021-12-14 13:39:49 +0800

    useReducer

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,24 +1,43 @@
-import { FC, ReactElement, useState, useEffect, useCallback } from 'react';
+import {
+  FC,
+  ReactElement,
+  useState,
+  useEffect,
+  useCallback,
+  useReducer,
+} from 'react';
 
 import TdInput from './Input';
 import TdList from './List';
 
-import { ITodo } from './typings';
+import { ITodo, IState, ACTION_TYPE } from './typings';
+import { todoReducer } from './reducer';
+
+const initialState: IState = {
+  todoList: [],
+};
 
 const TodoList: FC = (): ReactElement => {
-  const [todoList, setTodoList] = useState<ITodo[]>([]);
+  // const [todoList, setTodoList] = useState<ITodo[]>([]);
+
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
   useEffect(() => {
-    console.log(todoList);
-  }, [todoList]);
+    console.log(state.todoList);
+  }, [state.todoList]);
 
   const addTodo = useCallback((todo: ITodo) => {
     console.log(todo);
-    setTodoList((todoList) => [...todoList, todo]);
+    // setTodoList((todoList) => [...todoList, todo]);
+    dispatch({
+      type: ACTION_TYPE.ADD_TODO,
+      payload: todo,
+    });
   }, []);
+
   return (
     <div className='todo-list'>
-      <TdInput addTodo={addTodo} todoList={todoList} />
+      <TdInput addTodo={addTodo} todoList={state.todoList} />
       <TdList />
     </div>
   );
